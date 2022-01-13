45537639402468c3fdbcd3111c0c818f652dfa9a
hxm <huangxiaoman@gmail.com>
2022-01-13 09:30:00 +0800

    refactoring logging

--- logging/logging.go
+++ logging/logging.go
@@ -1,110 +1,77 @@
 package logging
 
 import (
-	"fmt"
 	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+type Config struct {
+	ZapConfig *zap.Config `yaml:"zapConfig"`
+}
+
 var (
-	encConfig zapcore.EncoderConfig
-	encoder   zapcore.Encoder
-
-	stdout zapcore.WriteSyncer
-	stderr zapcore.WriteSyncer
-
-	level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
-
-	global Logging
+	global *zap.SugaredLogger
 )
 
 func init() {
-	encConfig = zap.NewDevelopmentEncoderConfig()
+	config := &Config{}
+
+	encConfig := zap.NewDevelopmentEncoderConfig()
 	encConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	encConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 		enc.AppendString(t.UTC().Format(time.StampMicro))
 	}
 
-	encoder = zapcore.NewConsoleEncoder(encConfig)
-
-	sout, closer, err := zap.Open("stdout")
-	if err != nil {
-		closer()
-		panic(fmt.Errorf("failed to initialize logger: %w", err))
+	config.ZapConfig = &zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development:      true,
+		Encoding:         "console",
+		EncoderConfig:    encConfig,
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
 	}
 
-	serr, closer, err := zap.Open("stderr")
-	if err != nil {
-		closer()
-		panic(fmt.Errorf("failed to initialize logger: %w", err))
-	}
-
-	stdout = sout
-	stderr = serr
-
-	global = NewLogging(NewLogger())
-}
-
-func SetLevel(l zapcore.Level) {
-	level.SetLevel(l)
-}
-
-func NewLogger(extraWs ...zapcore.WriteSyncer) *zap.Logger {
-	wss := append([]zapcore.WriteSyncer{stdout}, extraWs...)
-	ws := zapcore.NewMultiWriteSyncer(wss...)
-
-	core := zapcore.NewCore(encoder, ws, level)
-	return zap.New(core, zap.ErrorOutput(stderr), zap.AddStacktrace(zapcore.ErrorLevel))
-}
-
-type Logging struct {
-	//logger  *zap.Logger
-	sugared *zap.SugaredLogger
-}
-
-func NewLogging(logger *zap.Logger) Logging {
-	return Logging{
-		sugared: logger.Sugar(),
-	}
+	logger, _ := config.ZapConfig.Build()
+	global = logger.Sugar()
 }
 
 func Debugf(template string, args ...interface{}) {
-	global.sugared.Debugf(template, args)
+	global.Debugf(template, args)
 }
 
 func Debug(args ...interface{}) {
-	global.sugared.Debug(args)
+	global.Debug(args)
 }
 
 func Error(args interface{}) {
-	global.sugared.Error(args)
+	global.Error(args)
 }
 
 func Errorf(template string, args ...interface{}) {
-	global.sugared.Errorf(template, args...)
+	global.Errorf(template, args...)
 }
 
 func Info(str string) {
-	global.sugared.Info(str)
+	global.Info(str)
 }
 
 func Infof(template string, args ...interface{}) {
-	global.sugared.Infof(template, args...)
+	global.Infof(template, args...)
 }
 
 func Fatalw(msg string, keysAndValues ...interface{}) {
-	global.sugared.Fatalw(msg, keysAndValues...)
+	global.Fatalw(msg, keysAndValues...)
 }
 
 func Warning(err interface{}) {
-	global.sugared.Warn(err)
+	global.Warn(err)
 }
 
 func Warningf(template string, args ...interface{}) {
-	global.sugared.Warnf(template, args)
+	global.Warnf(template, args)
 }
 func Infow(msg string, keysAndValues ...interface{}) {
-	global.sugared.Infow(msg, keysAndValues)
+	global.Infow(msg, keysAndValues)
 }
